4fb8e766710420cb9039511a444f57c408bf4d56
saliou thioune <bayezalle995@gmail.com>
2023-10-26 09:42:01 +0000

    tontine

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import SignUp from "./Composantes/SignUp";
+import Login from "./Composantes/Login";
+import PrivateRoute from "./Auth/PrivateRoute";
+import Home from "./Composantes/Home";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+    <Route exact path="/" component={Home} /> 
+        <PrivateRoute exact path="/login" component={Login}/>
+        <Route exact path="/register" component={SignUp} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
